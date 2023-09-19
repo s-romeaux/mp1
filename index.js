c5be8d21f1646193c610ef4ffd308db560c06ade
S-Romeaux <sal.4845@gmail.com>
2023-09-19 16:51:36 -0700

    Chgd game answrs, start JS

---
+++ index.js
@@ -0,0 +1,14 @@
+const verbData = [
+    {
+    frenchInfinitive: "agir",
+    englishInfinitive: "to act",
+    englishPersonNumber: "i",
+    englishConjugated: "i act",
+    frenchConjugated: "j'agis",
+    order: 1,
+    grouping: "agir1"
+    }
+];
+
+console.log(verbData[0].frenchInfinitive);
+console.log(verbData[0].englishInfinitive);
